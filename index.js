db0413e82a1d81a2848e444cb1c4b933d16d20f6
shreya bajpai <shreya08052004@gmail.com>
2023-08-28 01:56:18 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+function check(buttonValue)
+{
+    switch(buttonValue){
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+        case "k":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+        break;
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        default:
+            //alert("Invalid Button");
+        break;
+    }
+}
+function animation(key)
+{
+    var activeButton = document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+var noOfButtons = document.querySelectorAll("button").length;
+for(var i=0;i<noOfButtons;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var buttonHolder = this.innerHTML;
+        check(buttonHolder);
+        animation(buttonHolder);
+       
+    });
+}
+document.addEventListener("keydown",function(KeyboardEvent){
+    var keyValue = KeyboardEvent.key;
+    check(keyValue);
+    animation(keyValue);
+});
+
